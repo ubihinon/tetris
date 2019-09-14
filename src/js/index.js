987d1b5463be5b508ed72bcd5025d59e979c2982
ubihinon <ubihinon1@gmail.com>
2019-09-14 12:00:08 +0700

    [main-logic] finished main part of the game

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,14 @@
 import Game from "./models/Game";
 import '../css/main.css';
 import View from "./view/View";
+import Controller from "./controller/controller";
 
 const root = document.querySelector('#canvas-container');
 
 const game = new Game();
 const view = new View(root, 480, 640, 20, 10);
+const controller = new Controller(game, view);
 
 window.game = game;
 window.view = view;
-
-document.addEventListener('keydown', event => {
-    switch (event.keyCode) {
-        case 37:
-            game.movePieceLeft();
-            view.renderMainScreen(game.getState());
-            break;
-        case 38:
-            game.rotatePiece();
-            view.renderMainScreen(game.getState());
-            break;
-        case 39:
-            game.movePieceRight();
-            view.renderMainScreen(game.getState());
-            break;
-        case 40:
-            game.movePieceDown();
-            view.renderMainScreen(game.getState());
-            break;
-    }
-});
+window.controller = controller;
