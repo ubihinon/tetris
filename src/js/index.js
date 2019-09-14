7c2ff8fc02918f4c14a3deda1fd391f35cf5ce6b
ubihinon <ubihinon1@gmail.com>
2019-09-14 10:23:41 +0700

    [create-model] added additional screens

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import View from "./view/View";
 const root = document.querySelector('#canvas-container');
 
 const game = new Game();
-const view = new View(root, 320, 640, 20, 10);
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
 window.view = view;
@@ -14,19 +14,19 @@ document.addEventListener('keydown', event => {
     switch (event.keyCode) {
         case 37:
             game.movePieceLeft();
-            view.render(game.getState());
+            view.renderMainScreen(game.getState());
             break;
         case 38:
             game.rotatePiece();
-            view.render(game.getState());
+            view.renderMainScreen(game.getState());
             break;
         case 39:
             game.movePieceRight();
-            view.render(game.getState());
+            view.renderMainScreen(game.getState());
             break;
         case 40:
             game.movePieceDown();
-            view.render(game.getState());
+            view.renderMainScreen(game.getState());
             break;
     }
 });
