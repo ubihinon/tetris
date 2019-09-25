355b9ad476237d3697c1f57c5339a7412479b615
ubihinon <ubihinon1@gmail.com>
2019-09-25 07:49:15 +0700

    [refactoring] ttf and svg fonts were added

--- webpack/fonts.js
+++ webpack/fonts.js
@@ -3,7 +3,7 @@ module.exports = function () {
         module: {
             rules: [
                 {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
                     loader: 'file-loader',
                     options: {
                         name: 'fonts/[name].[ext]',
